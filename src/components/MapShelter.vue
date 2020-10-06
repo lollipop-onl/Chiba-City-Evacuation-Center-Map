9a1a5e5a8ba0b42039be798130210d4067193d9a
tamuraryoya <tamuraryoya@team-lab.com>
2020-10-06 19:13:38 +0900

    ✨ 避難所情報にアニメーションを追加

--- src/components/MapShelter.vue
+++ src/components/MapShelter.vue
@@ -1,23 +1,29 @@
 <template>
-  <div
-    v-show="shelterId != null"
-    class="map-shelter-card"
-  >
-    <template
-      v-for="shelter in shelters"
-      :key="shelter.id"
+  <transition name="card">
+    <div
+      v-show="shelterId != null"
+      class="map-shelter-wrapper"
     >
-      <div
-        v-if="shelter.id === shelterId"
-        class="shelter"
-      >
-        {{ shelter.name }}
-        <br />
-        〒{{ shelter.postalCode }} 千葉県{{ shelter.address }}
+      <div class="card map-shelter-card">
+        <template
+          v-for="shelter in shelters"
+          :key="shelter.id"
+        >
+          <transition name="list">
+            <div
+              v-if="shelter.id === shelterId"
+              class="shelter"
+            >
+              {{ shelter.name }}
+              <br />
+              〒{{ shelter.postalCode }} 千葉県{{ shelter.address }}
+            </div>
+          </transition>
+        </template>
+        <RouterLink to="/map">閉じる</RouterLink>
       </div>
-    </template>
-    <RouterLink to="/map">閉じる</RouterLink>
-  </div>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -36,23 +42,22 @@ export default defineComponent({
       default: null,
     }
   },
-  setup(props) {
-    console.log(props.shelterId, props.shelters.find((shelter) => shelter.id === props.shelterId));
-  },
 });
 </script>
 
 <style lang="scss" scoped>
 @import 'resources';
 
-.map-shelter-card {
+$breakpoint: 600px;
+
+.map-shelter-wrapper {
   position: absolute;
   top: 0;
   left: 0;
   z-index: 1000;
   width: 400px;
   height: calc(var(--vh, 1vh) * 100 - 64px);
-  background: #f5f5f5;
+  overflow: hidden;
   box-shadow:
     0 0 2.2px rgba(0, 0, 0, 0.034),
     0 0 5.3px rgba(0, 0, 0, 0.048),
@@ -61,7 +66,7 @@ export default defineComponent({
     0 0 33.4px rgba(0, 0, 0, 0.086),
     0 0 80px rgba(0, 0, 0, 0.12);
 
-  @media (max-width: 600px) {
+  @media (max-width: $breakpoint) {
     top: auto;
     bottom: 0;
     width: 100%;
@@ -76,5 +81,51 @@ export default defineComponent({
       0 0 33.4px rgba(0, 0, 0, 0.086),
       0 0 80px rgba(0, 0, 0, 0.12);
   }
+}
+
+.map-shelter-card {
+  width: 400px;
+  height: calc(var(--vh, 1vh) * 100 - 64px);
+  background: #f5f5f5;
+
+  @media (max-width: $breakpoint) {
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+  }
+}
+
+.card-enter-active,
+.card-leave-active {
+  transition: opacity 0.3s ease, width 0.3s ease;
+
+  & > .card {
+    transition: transform 0.3s ease;
+  }
+}
+
+.card-enter-from,
+.card-leave-to {
+  width: 370px;
+  opacity: 0;
+
+  & > .card {
+    transform: translate3d(-30px, 0, 0);
+  }
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+}
+
+.list-leave-active {
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
