5ee93547a5e2c5c8fb0577f8a8aed5479121c3d0
tamuraryoya <tamuraryoya@team-lab.com>
2020-10-06 10:02:18 +0900

    🔨 不要なログを削除

--- src/utils/checkCoordinateAvailability.ts
+++ src/utils/checkCoordinateAvailability.ts
@@ -8,8 +8,6 @@ import { AVAILABLE_COORDINATE_RANGE } from '../const';
 export const checkCoordinateAvailability = (latitude: number, longitude: number): boolean => {
   const [[north, west], [south, east]] = AVAILABLE_COORDINATE_RANGE;
 
-  console.log([[north, west], [south, east]], latitude, longitude);
-
   return (
     north >= latitude &&
     south <= latitude &&
